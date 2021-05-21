de96f213657827cd38bee7c5a4d7bf4c4237d784
Makoto <makoto@emurasoft.com>
2021-05-21 16:58:21 -0700

    Tree

---
+++ src/main/java/Tree.java
@@ -0,0 +1,110 @@
+import java.util.Optional;
+
+public class Tree<K extends Comparable<K>> {
+	private enum Color {
+		RED,
+		BLACK;
+
+		Color negate() {
+			if (this == RED) {
+				return BLACK;
+			}
+
+			return RED;
+		}
+	}
+
+	private class Node {
+		final K key;
+		// value is the key of movie database
+		Integer value;
+		Node left;
+		Node right;
+		Color color;
+
+		Node(K key, Integer value, Color color) {
+			this.key = key;
+			this.value = value;
+			this.left = null;
+			this.right = null;
+			this.color = color;
+		}
+	}
+
+	private Node root;
+
+	public Optional<Integer> get(K key) {
+		Node curr = root;
+		while (curr != null) {
+			int cmp = key.compareTo(curr.key);
+			if (cmp < 0) {
+				curr = curr.left;
+			} else if (cmp > 0) {
+				curr = curr.right;
+			} else {
+				return Optional.of(curr.value);
+			}
+		}
+		return Optional.empty();
+	}
+
+	public void insert(K key, Integer value) {
+		root = insert(root, key, value);
+		root.color = Color.BLACK;
+	}
+
+	private Node insert(Node root, K key, Integer value) {
+		if (root == null) {
+			return new Node(key, value, Color.RED);
+		}
+
+		int cmp = key.compareTo(root.key);
+		if (cmp < 0) {
+			root.left = insert(root.left, key, value);
+		} else if (cmp > 0) {
+			root.right = insert(root.right, key, value);
+		} else {
+			root.value = value;
+		}
+
+		if (isRed(root.right) && !isRed(root.left)) {
+			root = rotateLeft(root);
+		}
+		if (isRed(root.left) && isRed(root.left.left)) {
+			root = rotateRight(root);
+		}
+		if (isRed(root.left) && isRed(root.right)) {
+			flipColors(root);
+		}
+
+		return root;
+	}
+
+	private boolean isRed(Node node) {
+		return node != null && node.color == Color.RED;
+	}
+
+	private Node rotateRight(Node node) {
+		Node newRoot = node.left;
+		node.left = newRoot.right;
+		newRoot.right = node;
+		newRoot.color = newRoot.right.color;
+		newRoot.right.color = Color.RED;
+		return newRoot;
+	}
+
+	private Node rotateLeft(Node node) {
+		Node newRoot = node.right;
+		node.right = newRoot.left;
+		newRoot.left = node;
+		newRoot.color = newRoot.left.color;
+		newRoot.left.color = Color.RED;
+		return newRoot;
+	}
+
+	private void flipColors(Node node) {
+		node.color = node.color.negate();
+		node.left.color = node.left.color.negate();
+		node.right.color = node.right.color.negate();
+	}
+}
---
+++ src/main/java/TreeTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+public class TreeTest {
+	@Test
+	public void test() {
+		var tree = new Tree<String>();
+		assertEquals(Optional.empty(), tree.get("a"));
+		tree.insert("a", 0);
+		assertEquals(Optional.of(0), tree.get("a"));
+		tree.insert("b", 1);
+		assertEquals(Optional.of(1), tree.get("b"));
+		tree.insert("c", 2);
+		assertEquals(Optional.of(2), tree.get("c"));
+		tree.insert("a", 3);
+		assertEquals(Optional.of(3), tree.get("a"));
+	}
+}
