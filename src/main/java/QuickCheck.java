1690d25a55ddc843b93df71233c016bec64d337a
Makoto <makoto@emurasoft.com>
2021-05-22 10:23:49 -0700

    Move quick check tests

--- src/main/java/QuickCheck.java
+++ src/main/java/QuickCheck.java
@@ -5,15 +5,26 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(JUnitQuickcheck.class)
-public class HashMapQuickCheck {
+public class QuickCheck {
 	@Property
-	public void test(int[] items) {
+	public void hashMap(int[] items) {
 		var map = new HashMap<Integer, Integer>(items.length);
 		for (var item : items) {
 			map.insert(item, item);
 		}
 		for (var item : items) {
 			assertTrue(map.get(item).isPresent());
+		}
+	}
+
+	@Property
+	public void tree(int[] items) {
+		var tree = new Tree<Integer>();
+		for (var item : items) {
+			tree.insert(item, item);
+		}
+		for (var item : items) {
+			assertTrue(tree.get(item).isPresent());
 		}
 	}
 }
